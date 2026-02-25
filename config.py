b2d20d413f8d39c080246e0602d88c37edc7aeff
Manugowda08 <117275860+manugowda08@users.noreply.github.com>
2026-02-25 08:18:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from supabase import create_client, Client
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('sb_secret_umLsvBQc91dgOvZspodNtw_wUn4eIzQ')
+    SECRET_KEY = os.environ.get('sb_secret_umLsvBQc91dgOvZspodNtw_wUn4eIzQ') or 'dev-change-in-production'
     SUPABASE_URL = os.environ.get('https://ulyocqshdtqgwrcwgxik.supabase.co')
     SUPABASE_ANON_KEY = os.environ.get('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVseW9jcXNoZHRxZ3dyY3dneGlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NTAxNTUsImV4cCI6MjA4NzUyNjE1NX0.7JbBhOx_-8aznbzj2EEu5cMNPJ5c_J0r-UdRZlEUUOk')
     
