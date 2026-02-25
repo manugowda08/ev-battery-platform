b3385a428c0cb12f409b4c93601eb49662d51177
Manugowda08 <117275860+manugowda08@users.noreply.github.com>
2026-02-25 07:40:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
-from dotenv import load_dotenv
+from supabase import create_client, Client
 
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-mode-only-change-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('sb_secret_umLsvBQc91dgOvZspodNtw_wUn4eIzQ')
+    SUPABASE_URL = os.environ.get('https://ulyocqshdtqgwrcwgxik.supabase.co')
+    SUPABASE_ANON_KEY = os.environ.get('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVseW9jcXNoZHRxZ3dyY3dneGlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NTAxNTUsImV4cCI6MjA4NzUyNjE1NX0.7JbBhOx_-8aznbzj2EEu5cMNPJ5c_J0r-UdRZlEUUOk')
+    
+    @staticmethod
+    def get_supabase() -> Client:
+        return create_client(Config.SUPABASE_URL, Config.SUPABASE_ANON_KEY)
